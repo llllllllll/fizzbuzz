a711bebc2c2e67f9010c10104564ebbca9bbc451
Tom Norris <tnorris@users.noreply.github.com>
2018-02-12 16:21:56 -0500

    Improves fizzbuzz.rb's golf score
    Adds:
    - functional programming
    - nested ternary operators
    - multiple assignment

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,2 +1,1 @@
-(1..100).each {|i| (y=(i%3==0));(x=(i%5==0));if (y && x) then puts "FizzBuzz" elsif y then puts "Fizz" elsif x then puts "Buzz" else puts i end}
-
+puts (1..100).map{|i|y,x=i%3==0,i%5==0;y&&x ? 'FizzBuzz' : y ? 'Fizz' : x ? 'Buzz' : i}
